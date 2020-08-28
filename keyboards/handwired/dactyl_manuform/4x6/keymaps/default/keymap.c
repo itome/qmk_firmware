2f42092d872f43278c287f5bc10ae461a07d07da
Takeshi Tsukamoto <itometeam@gmail.com>
2020-08-28 12:17:46 +0900

    Update keymaps

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
     KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
                     _______,KC_LGUI,                                            KC_RALT,_______,                   \
-                                    LOWER  ,KC_SPC,             KC_ENT, RAISE,                        \
-                                    KC_TAB, KC_HOME,            KC_END, KC_DEL,                                    \
+                                    KC_LGUI,LOWER ,             RAISE,  KC_RALT,                        \
+                                    KC_TAB, KC_SPC,             KC_ENT, KC_DEL,                                    \
                                     KC_BSPC,KC_GRV,             KC_LGUI,KC_LALT                                    \
 ),
 
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,_______,_______,_______,_______,_______,            KC_LEFT,KC_DOWN,KC_UP  ,KC_RIGHT,_______,_______,  \
     KC_LSFT,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,   \
                     _______,KC_LGUI,                                            KC_RALT,_______,                   \
-                                            LOWER,  KC_SPC,     KC_ENT ,RAISE  ,                                    \
-                                            _______,_______,    _______,_______,                                   \
+                                            KC_LGUI,LOWER ,     RAISE  ,KC_RALT,                                    \
+                                            _______,KC_SPC ,    KC_ENT ,_______,                                   \
                                             _______,_______,    _______,_______                                    \
 ),
 
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,_______,_______,_______,_______,_______,            KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV ,   \
     KC_LSFT,_______,_______,_______,_______,_______,            KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD ,  \
                     _______,KC_LGUI,                                            KC_RALT,_______,                   \
-                                            RAISE,  KC_SPC,     KC_ENT ,RAISE  ,                                    \
-                                            _______,_______,    _______,_______,                                   \
+                                            KC_LGUI,LOWER  ,    RAISE  ,KC_RALT,                                    \
+                                            _______,KC_SPC ,    KC_ENT,_______,                                   \
                                             _______,_______,    _______,_______                                    \
 )
 };
